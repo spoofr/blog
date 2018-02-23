7227c1a2ec698361ebc2fa95fe104a525f4b735c
sinyadal <v0xe7y2cl@protonmail.com>
2018-02-23 19:55:19 +0800

    Setup Controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Category;
 
 class CategoryController extends Controller
 {
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Category;
 
 class PostController extends Controller
 {
