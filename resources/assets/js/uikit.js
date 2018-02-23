47087e7ea5c56d641d69512223ccc419e90d0061
sinyadal <v0xe7y2cl@protonmail.com>
2018-02-23 18:26:14 +0800

    Setup UIKit

--- resources/assets/js/uikit.js
+++ resources/assets/js/uikit.js
@@ -1,17 +1,12 @@
 
 window._ = require('lodash');
-window.Popper = require('popper.js').default;
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
+import Icons from 'uikit/dist/js/uikit-icons';
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
+    window.UIKit = require('uikit');
+    UIKit.use(Icons);
 } catch (e) {}
 
 /**
