faded2d9bd37dfb43893830d9c000ac5d327c312
sinyadal <v0xe7y2cl@protonmail.com>
2018-02-23 19:16:24 +0800

    Configure Post model

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+    use SoftDeletes;
+
+    // Tell laravel to treat $deleted_at column as date
+    protected $dates = ['deleted_at'];
+
+    // Variable for mass asignment
+    protected $fillable = [
+        'title', 'content', 'category_id', 'slug', 'featured_image',
+    ];
+
+    // Accessor for $featured_image
+    public function getFeaturedImageAttribute($featured_image)
+    {
+        return asset('/images/posts/' . $featured_image);
+    }
+
+    //  Category table relationship
+    public function categories()
+    {
+        return $this->belongsTo('App\Categories');
+    }
 }
