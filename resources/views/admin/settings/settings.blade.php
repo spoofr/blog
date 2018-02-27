98e896331beac92a0b52450a8a7f0c427e402bc5
sinyadal <v0xe7y2cl@protonmail.com>
2018-02-27 16:31:01 +0800

    Change home route name

--- resources/views/admin/settings/settings.blade.php
+++ resources/views/admin/settings/settings.blade.php
@@ -8,39 +8,37 @@
             @csrf
 
             <div class="uk-margin">
-                    <label class="uk-form-label">Site's Name:</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input" type="text" name="site_name" value="{{ $setting->site_name }}"> @if ($errors->has('site_name'))
-                        <div class="uk-margin uk-text-danger">
-                            <p>{{ $errors->first('site_name') }}</p>
-                        </div>
-                        @endif
+                <label class="uk-form-label">Site's Name:</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" type="text" name="site_name" value="{{ $setting->site_name }}"> @if ($errors->has('site_name'))
+                    <div class="uk-margin uk-text-danger">
+                        <p>{{ $errors->first('site_name') }}</p>
                     </div>
+                    @endif
                 </div>
+            </div>
 
             <div class="uk-margin">
-                    <label class="uk-form-label">Address:</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input" type="text" name="address" value="{{ $setting->address }}"> @if ($errors->has('address'))
-                        <div class="uk-margin uk-text-danger">
-                            <p>{{ $errors->first('address') }}</p>
-                        </div>
-                        @endif
+                <label class="uk-form-label">Address:</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" type="text" name="address" value="{{ $setting->address }}"> @if ($errors->has('address'))
+                    <div class="uk-margin uk-text-danger">
+                        <p>{{ $errors->first('address') }}</p>
                     </div>
+                    @endif
                 </div>
+            </div>
 
             <div class="uk-margin">
-                    <label class="uk-form-label">Contact Number:</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input" type="text" name="contact_number" value="{{ $setting->contact_number }}"> @if ($errors->has('contact_number'))
-                        <div class="uk-margin uk-text-danger">
-                            <p>{{ $errors->first('contact_number') }}</p>
-                        </div>
-                        @endif
+                <label class="uk-form-label">Contact Number:</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" type="text" name="contact_number" value="{{ $setting->contact_number }}"> @if ($errors->has('contact_number'))
+                    <div class="uk-margin uk-text-danger">
+                        <p>{{ $errors->first('contact_number') }}</p>
                     </div>
+                    @endif
                 </div>
-
-
+            </div>
 
             <div class="uk-margin">
                 <label class="uk-form-label">Contact Email:</label>
@@ -52,7 +50,6 @@
                     @endif
                 </div>
             </div>
-
 
             <div class="uk-margin">
                 <button type="submit" class="uk-button uk-button-primary uk-float-right">Update Settings!</button>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/admin/home', 'HomeController@index')                                       ->name('home');
+Route::get('/admin/dashboard', 'HomeController@index')                                       ->name('dashboard');
 
 // Auth middleware
 Route::middleware('auth')->group(function (){
