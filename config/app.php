91af9f5a37815c55222ca5c926112a9be4e6da8a
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-14 11:47:07 +0800

    Remove Image intervsnetion in app.php

--- config/app.php
+++ config/app.php
@@ -146,7 +146,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -209,7 +208,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
